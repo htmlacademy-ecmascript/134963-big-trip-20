31491d21f4033d57e0731a4fd8a739b335a8910d
kk1089 <kk1089@mail.ru>
2023-07-04 00:01:19 +0700

    1.88

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -1,9 +1,6 @@
-import NewTaskButtonView from '../view/new-task-button-view.js';
 import TripPointView from '../view/trip-point.js';
 import TripPointsList from '../view/trip-point-list.js';
 import NewPointView from '../view/new-point-view.js';
-import NewPointWithoutOffersView from '../view/new-point-without-offers-view.js';
-import NewPointWithoutDestinationS from '../view/new-point-without-destination-view.js';
 import SortView from '../view/sort-view.js';
 import { render } from '../render.js';
 
@@ -12,18 +9,16 @@ export default class TripPresenter {
   sortComponent = new SortView();
 
   constructor({tripContainer}) {
-    this.TripContainer = tripContainer;
+    this.tripContainer = tripContainer;
   }
 
   init() {
-    render(this.tripComponent, this.tripContainer);
     render(this.sortComponent, this.tripContainer);
-
+    render(this.tripComponent, this.tripContainer);
+    render (new NewPointView(), this.tripComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
       render(new TripPointView(), this.tripComponent.getElement());
     }
-
-    render(new NewTaskButtonView(), this.tripComponent.getElement());
   }
 }
