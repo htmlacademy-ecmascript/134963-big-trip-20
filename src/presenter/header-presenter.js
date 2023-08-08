099f86ef43f8e15540ab9d44e158663e13be2ca7
kk1089 <kk1089@mail.ru>
2023-08-09 02:09:19 +0700

    скрыл heder-prisenter

--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -1,9 +1,9 @@
-import FilterView from '../view/filter-view.js';
-import TripInfoView from '../view/trip-info-view.js';
-import NewTaskButtonView from '../view/new-task-button-view.js';
-import { render, remove, replace } from '../framework/render.js';
+// import FilterView from '../view/filter-view.js';
+// import TripInfoView from '../view/trip-info-view.js';
+// import NewTaskButtonView from '../view/new-task-button-view.js';
+// import { render, remove, replace } from '../framework/render.js';
 
 
-export default class TripPresenter{
+// export default class TripPresenter{
 
-}
+// }
