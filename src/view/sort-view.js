442572fc4a0cdb52f5b265bdf83285c066f289b5
kk1089 <kk1089@mail.ru>
2023-09-06 12:26:17 +0700

    7.5 sort-view Шаблон сортировки научим учитывать

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,7 +1,7 @@
 import AbstractView from '../framework/view/abstract-view.js';
 import { SortType } from '../const.js';
 
-const createSortTemplate = () => (
+const createSortTemplate = (currentSortType) => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
       <div class="trip-sort__item  trip-sort__item--day">
         <input 
@@ -12,6 +12,7 @@ const createSortTemplate = () => (
         name="trip-sort" 
         value="sort-day"
         data-sort-type="${SortType.DEFAULT}"
+        ${currentSortType === SortType.DEFAULT ? 'checked' : ''}
         >
         <label class="trip-sort__btn" for="sort-day">Day</label>
       </div>
@@ -36,6 +37,7 @@ const createSortTemplate = () => (
           name="trip-sort" 
           value="sort-time"
           data-sort-type="${SortType.TIME_DESC}"
+          ${currentSortType === SortType.TIME_DESC ? 'checked' : ''}
         >
         <label class="trip-sort__btn" for="sort-time">Time</label>
       </div>
@@ -48,6 +50,7 @@ const createSortTemplate = () => (
           name="trip-sort" 
           value="sort-price"
           data-sort-type="${SortType.PRICE_DESC}"
+          ${currentSortType === SortType.PRICE_DESC ? 'checked' : ''}
           checked
         >
         <label class="trip-sort__btn" for="sort-price">Price</label>
@@ -69,9 +72,11 @@ const createSortTemplate = () => (
 
 export default class SortView extends AbstractView {
   #handleSortTypeChange = null;
+  #currentSortType = null;
 
-  constructor({ onSortTypeChange }) {
+  constructor({ onSortTypeChange, currentSortType }) {
     super();
+    this.#currentSortType = currentSortType;
     this.#handleSortTypeChange = onSortTypeChange;
     this.element.addEventListener('change', this.#sortTypeChangeHandler);
   }
@@ -82,6 +87,6 @@ export default class SortView extends AbstractView {
   };
 
   get template() {
-    return createSortTemplate();
+    return createSortTemplate(this.#currentSortType);
   }
 }
