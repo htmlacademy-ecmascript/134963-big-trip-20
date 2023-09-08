2b439ee5dbdbd397cebdbc50532a8469fff5e4b2
kk1089 <kk1089@mail.ru>
2023-09-09 01:19:17 +0700

    7.8

--- src/main.js
+++ src/main.js
@@ -6,11 +6,20 @@ import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destination-model.js';
 import MockService from './service/mock-service.js';
 import FilterModel from './model/filter-model.js';
+import FilterView from './view/filter-view.js';
+import {render} from './framework/render.js';
 
+const filters = [
+  {
+    type: 'all',
+    count: 0,
+  },
+];
 
 const tripContainer = document.querySelector('.trip-events');
 const tripMainElement = document.querySelector('.trip-main');
 const tripFilterContainer = document.querySelector('.trip-controls__filters');
+// const filterContainer = document.querySelector('');
 
 const mockService = new MockService();
 const pointsModel = new PointsModel(mockService);
@@ -32,6 +41,13 @@ const headerPresenter = new HeaderPresenter({
   filterContainer: tripFilterContainer,
 });
 
+
+render(new FilterView({
+  filters,
+  currentFilterType: 'all',
+  onFilterTypeChange: () => {}
+
+}), tripMainElement);
 
 headerPresenter.init();
 tripPresenter.init();
--- src/presenter/header-presenter.js
+++ src/presenter/header-presenter.js
@@ -1,28 +1,18 @@
-import FilterView from '../view/filter-view.js';
 import TripInfoView from '../view/trip-info-view.js';
 import NewTaskButtonView from '../view/new-task-button-view.js';
-import { generateFilter } from '../mock/filter.js';
 import { render, RenderPosition } from '../framework/render';
 
 
 export default class HeaderPresenter {
   #headerContainer = null;
-  #filterContainer = null;
 
-  #filters = [];
 
-  #pointsModel = null;
-
-  constructor ({headerContainer, pointsModel, filterContainer}) {
+  constructor ({headerContainer}) {
     this.#headerContainer = headerContainer;
-    this. #filterContainer = filterContainer;
 
-    this.#pointsModel = pointsModel;
-    this.#filters = generateFilter(this.#pointsModel.points);
   }
 
   init() {
-    this.#renderFilters();
     this.#renderNewTaskButtonView();
     this.#renderTripInfoView();
 
@@ -37,7 +27,4 @@ export default class HeaderPresenter {
     render(new NewTaskButtonView(),this.#headerContainer);
   }
 
-  #renderFilters() {
-    render(new FilterView({ filters: this.#filters }), this.#filterContainer);
-  }
 }
