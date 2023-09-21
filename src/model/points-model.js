c8cae24d524157117fff435a8b4b51d927bc92ad
kk1089 <kk1089@mail.ru>
2023-09-21 23:14:43 +0700

    8.1.3

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -9,8 +9,8 @@ export default class PointsModel extends Observable {
     this.#service = service;
     this.#points = this.#service.getPoints();
 
-    this.#service.tasks.then((point) => {
-      console.log(point);
+    this.#service.points.then((point) => {
+      console.log(point.map(this.#adaptToClient));
       // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
       // а ещё на сервере используется snake_case, а у нас camelCase.
       // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
@@ -59,5 +59,23 @@ export default class PointsModel extends Observable {
     ];
 
     this._notify(updateType);
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {
+      ...point,
+      dateFrom: point['date_from'] !== null ? new Date(point['date_from']) : point['date_from'],
+      dateTo: point['date_to'] !== null ? new Date(point['date_to']) : point['date_to'],
+      basePrice: point['base_price'],
+      isFavorite: point['is_favorite'],
+    };
+
+    delete adaptedPoint['date_from'];
+    delete adaptedPoint['date_to'];
+    delete adaptedPoint['base_price'];
+    delete adaptedPoint['is_favorite'];
+
+    return adaptedPoint;
+
   }
 }
--- src/service/trip-api-service.js
+++ src/service/trip-api-service.js
@@ -11,12 +11,31 @@ export default class TripApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       Method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {
+      ...point,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null, // На сервере дата хранится в ISO формате
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null, // На сервере дата хранится в ISO формате
+      'base_price': point.basePrice,
+      'is_favorite': point.isFavorite,
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo ;
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
+
   }
 }
